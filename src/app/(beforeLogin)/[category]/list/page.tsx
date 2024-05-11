4f5c9b28f38fdb4b5430d0f30ba44243ec769beb
minhyepark <minhyepark.dev@gmail.com>
2024-05-12 00:16:21 +0900

    edit: 카테고리 리스트 페이지 수정

--- src/app/(beforeLogin)/[category]/list/page.tsx
+++ src/app/(beforeLogin)/[category]/list/page.tsx
@@ -1,10 +1,3 @@
-import Button from '@/components/button/Button'
-
 export default function Home() {
-  return (
-    <main>
-      카테고리 리스트 페이지
-      <Button />
-    </main>
-  )
+  return <main>카테고리 리스트 페이지</main>
 }
