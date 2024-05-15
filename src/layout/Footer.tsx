03bd3bd3586d6373563f25d7c7a5b6f34f480f78
minhyepark <minhyepark.dev@gmail.com>
2024-05-15 23:10:13 +0900

    edit: header,footer에 로고 추가

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -1,8 +1,11 @@
+import Image from 'next/image'
+import Logo from '../../public/logo.png'
+
 export default function Footer() {
   return (
     <footer className="footer">
       <h2 className="hidden">하단 메뉴</h2>
-      <img src="https://placehold.co/100x50" alt="하단 로고" />
+      <Image src={Logo} alt="로고" width="150" />
       <address className="content3">
         <p>주소: 서울특별시 강남구 테헤란로 427 위워크 선릉</p>
         <p>대표: 홍길동</p>
--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,7 +1,12 @@
+import Image from 'next/image'
+import Logo from '../../public/logo.png'
+
 export default function Header() {
   return (
     <header className="header">
-      <h1>서비스이름&로고</h1>
+      <h1>
+        <Image src={Logo} alt="로고" width="150" />
+      </h1>
     </header>
   )
 }
