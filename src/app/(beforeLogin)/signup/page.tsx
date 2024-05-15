788780e3ea41b58697e4c8c6e798fef37b055f34
minhyepark <minhyepark.dev@gmail.com>
2024-05-15 13:49:16 +0900

    edit: signup page

--- src/app/(beforeLogin)/signup/page.tsx
+++ src/app/(beforeLogin)/signup/page.tsx
@@ -1,3 +1,31 @@
+import Button from '@/components/button/Button'
+import Input from '@/components/input/Input'
+import InnerLayout from '@/components/layouts/InnerLayout'
+
 export default function Home() {
-  return <main>회원가입 페이지</main>
+  return (
+    <main>
+      <InnerLayout>
+        <h1 className="h1 marginBottom32">Sign Up</h1>
+        <form>
+          <Input label="이메일" type="email" placeHolder="이메일" isRequired />
+          <Input
+            label="비밀번호"
+            type="password"
+            placeHolder="비밀번호"
+            isRequired
+          />
+          <Input
+            label="비밀번호 확인"
+            type="password"
+            placeHolder="비밀번호 확인"
+            isRequired
+          />
+          <Button width="default" btnStyle="bgGreen" className="bold">
+            회원가입
+          </Button>
+        </form>
+      </InnerLayout>
+    </main>
+  )
 }
